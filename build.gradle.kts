75d25b4dd2d5ac29892f4e5efd6084e4ed7010b9
Xavier Jimenez <xavijimenezmulet@macaqueconsulting.com>
2023-03-15 12:58:56 +0100

    Removed unused plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
         classpath (Depends.kotlinPlugin)
         classpath (Depends.hiltGradlePlugin)
         classpath(Depends.kotlinSymbolProcessing)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     }
 }
 
